4b232883e8a7d1ca92562aa9618289e2cb4a19dd
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-05-05 20:44:00 +0530

    Add Sorting

--- L25/Sorting.cpp
+++ L25/Sorting.cpp
@@ -3,14 +3,30 @@
 using namespace std;
 void bubbleSort(vector<int>& arr) {
     int n = arr.size();
-    for (int i = 0; i < n-1; i++) {
+    for (int i = 0; i < n-1; i++) {//=> n
         //Every Pass
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < n - i - 1; j++) { //=> for every i here have n time ittration n*n   
             // Compare adjacent elements
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
             }
         }
+    }
+}
+// Time Complexity: O(n^2)
+
+void selectionSort(vector<int>& arr){
+    int n = arr.size();
+    for(int i=0;i<n-1;i++){
+        int min = INT_MAX;
+        int minIndex = -1;
+        for(int j=i;j<n;j++){
+            if(arr[j]<min){
+                min = arr[j];
+                minIndex = j;
+            }
+        }
+        swap(arr[i],arr[minIndex]);
     }
 }
 int main(){
